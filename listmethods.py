98c7f91e89c488cc08075c02b14106834b7d08b6
vinilpaulp@gmail.com <vinilpaulp@gmail.com>
2025-05-02 14:52:20 +0400

    committed

--- listmethods.py
+++ listmethods.py
@@ -1,6 +1,8 @@
-fruits=["appale","banana","cherry","orange"]
-# print("banana" in fruitslist)
-# l1=fruitslist[-4:-1]
-# print(l1)
-l2-list=fruits.reverse()
-l2-list
+fruits=["apple","banana","cherry","orange"]
+print("banana" in fruits)
+l1=fruits[-4:-1:2] 
+l2=l1[::-1]
+print("list1 ",l1)
+print("list2 ",l2)
+l3=list(reversed(fruits))
+print("list 3",l3)
